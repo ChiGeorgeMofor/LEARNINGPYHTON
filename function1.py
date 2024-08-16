6db989eb79e884fada046d5a547e645552d352cd
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-08-16 18:36:48 +0100

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,14 @@
+def things():
+    print('Hello')
+    print('Friend')
+
+things()
+print('Zip Space')
+things()   
+
+
+big = max('Hello world')
+print(big)
+
+tiny = min('Hello Mhen')
+print(tiny)
---
+++ tryecept.py
@@ -0,0 +1,9 @@
+astr = 'Bob'
+try:
+    print('Hello')
+    intr = int(astr)
+    print('There')
+except:
+    istr = -1
+
+print('Done', istr)
