611774776629d4eedf42ad2e3355ef0158b9a768
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-08-26 21:42:15 +0100

    Update averageloop.py

--- averageloop.py
+++ averageloop.py
@@ -3,6 +3,6 @@ sum = 0
 print('Before', count, sum) 
 for value in [9, 41, 12, 3, 74, 15]: 
     count = count + 1
-    sum = sum + value
+    sum = sum + value 
     print(count, sum, value)
 print('After', count, sum, sum / count)
